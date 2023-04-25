79e1cb5318888d51efebbfec50ca63edfc725a25
Ana Studler <anistudler@gmail.com>
2023-04-25 18:37:55 -0300

    models sin const

--- back/src/models/Admin.js
+++ back/src/models/Admin.js
@@ -1,32 +1,34 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 //! Agregar propiedades de admin.
 
 module.exports = (sequelize) => {
-  const User = sequelize.define('user', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-   },
-    mail: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-   },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-    surname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-  },
-  { timestamps: false }
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      mail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
   );
 };
--- back/src/models/User.js
+++ back/src/models/User.js
@@ -1,32 +1,34 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  const User = sequelize.define('user', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-   },
-    mail: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-   },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-    surname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-   },
-  },
-  { timestamps: false }
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      mail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
   );
 };
