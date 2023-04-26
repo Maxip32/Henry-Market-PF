a14141f1947e0710f81c93cf15ab8b790b45eea6
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-04-26 17:34:31 -0300

    SaveData

--- back/src/controllers/Products/saveData.js
+++ back/src/controllers/Products/saveData.js
@@ -16,7 +16,7 @@ const uploadImage = async (imagePath) => {
         console.error(error);
     }
 }
-const getAssetInfo = async () => {
+const getApiData = async () => {
     try {
         const result = await cloudinary.api.resources({max_results: 500})
         // res es un array de objetos con la propiedad url y url_secure que nos interesan
@@ -27,31 +27,33 @@ const getAssetInfo = async () => {
     }
 };
 
-//! Info API (cloudinary supongo)
-const getApiData = async () => {
-    try {
-        let products = [];
-        let apiData = await axios(/*CLOUDINARY*/);
-        products.push(apiData);
-        products = (await Promise.all(products)).map((a) => a.data.map((res) => {
-            return {
-                id: res.id,
-                image: res.image,
-            }
-        }));
-        let allProducts = [];
-        products.map((a) => {
-            allProducts = allProducts.concat(a)
-        });
-        return allProducts
-    } catch (error) {
-        return {error: error.message}
-    }
-};
+// const getApiData = async () => {
+//     try {
+//         let products = [];
+//         let apiData = await axios(/*CLOUDINARY*/);
+//         products.push(apiData);
+//         products = (await Promise.all(products)).map((a) => a.data.map((res) => {
+//             return {
+//                 id: res.id,
+//                 image: res.image,
+//             }
+//         }));
+//         let allProducts = [];
+//         products.map((a) => {
+//             allProducts = allProducts.concat(a)
+//         });
+//         return allProducts
+//     } catch (error) {
+//         return {error: error.message}
+//     }
+// };
 //! Info de la DB
 const getDbData = async () => {
     const res = Products.finAll({
-        include: {}
+        include: {
+            model: ProductsName,
+            attributes: ['name'],
+        }
     })
     return res
 };
