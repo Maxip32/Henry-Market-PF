ed58f342d016b29fc3e909f7501331c550ca2a38
Oscarcito122 <autlock64@gmail.com>
2023-04-26 19:00:49 -0300

    nav y search en proceso

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route,useLocation} from "react-router-dom"
 import LandingPage from './components/LandingPage/LandingPage';
 import Home from "./components/home/Home";
+import Nav from "./components/nav/Nav";
 function App() {
+      const location = useLocation()
+
   return (
     
     <div className='App'>
+          <div className="nav"> {location.pathname !== '/' && <Nav/>}</div>
       <Routes>
         <Route exact path='/' element={<LandingPage/>} />
         <Route exact path='/home' element={<Home/>} />
