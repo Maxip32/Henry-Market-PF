3e9a6cc20a6815cad0119b693b8fd24b97350e2b
Maxip32 <piolattoemanuel@gmail.com>
2023-04-26 14:27:16 -0300

    Front arreglado

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 import LandingPage from './components/LandingPage/LandingPage';
-//import Home from "./components/home/Home";
+import Home from "./components/home/Home";
 function App() {
   return (
     
     <div className='App'>
       <Routes>
         <Route exact path='/' element={<LandingPage/>} />
-        {/* <Route exact path='/home' element={<Home/>} /> */}
+        <Route exact path='/home' element={<Home/>} />
 
       </Routes>
       
--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
-
+import logo from "../Image/logo.png"
 export default function LandingPage() {
   return (
     <div className={styles.landingSection}>
       <div className={styles.landingBox}>
         <h1 className={`${styles.landingTitle} ${styles.welcome}`}>
-         
+         <img src={logo} alt="henry" />
         </h1>
         <Link to="/home">
           <button className={styles.landingButton}>Get In</button>
