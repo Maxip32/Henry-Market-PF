7131d0abcac56e26f327588ba4d521ee09171281
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-04-26 18:42:01 -0400

    feat: getAlls and getById ok

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,13 @@
-const { Router } = require("express");
+const {Router} = require("express");
 
 const productsRouter = require("./Products");
 const userRouter = require("./User");
+const shoppingCartRouter = require("./ShoppingCartRoute");
 
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/products", productsRouter);
+router.use("/shoppingCart", shoppingCartRouter);
 
 module.exports = router;
