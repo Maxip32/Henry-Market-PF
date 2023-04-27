169e5e694ea0e6a3d2a744673e465a28751110b9
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-04-27 10:06:02 -0300

    Rutas Prods

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -14,5 +14,33 @@ router.use("/users", userRouter);
 router.use("/products", productsRouter);
 router.use("/shoppingCart", shoppingCartRouter);
 
+router.get('/allproducts', async (req, res) => {
+    try {
+        const allProds = await getAllProducts();
+        res.status(200).json(allProds)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+router.get('/products/name', async (req, res) => {
+    //* Para buscar en Thunder ==> parameter name ---- value = nombre producto
+    const { name } = req.query;
+    try {
+        const result = await getByName(name);
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+router.get('/products/category', async (req, res) => {
+    const { category } = req.query;
+    try {
+        const result = await getByCategory(category);
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+
 
 module.exports = router;
