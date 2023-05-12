29f6585d27db6bbafe3f083447a8c158bf3a22a2
juanpr19216 <120424316+enano17711@users.noreply.github.com>
2023-05-12 10:49:42 -0400

    Update createOrder.js
    fix order creation to roolback in error case

--- back/src/services/orders/createOrder.js
+++ back/src/services/orders/createOrder.js
@@ -1,4 +1,4 @@
-const {Order, OrderDetail, ProductsName} = require("../../db");
+const {Order, OrderDetail, ProductsName, conn} = require("../../db");
 const mercadoPago = require("mercadopago")
 
 mercadoPago.configure({
@@ -24,40 +24,42 @@ const orderDataExample = {
 
 // create order - orderDetail and mercadoPago data, update products stock
 const createOrder = async (order) => {
+    const transaction = await conn.transaction() // start transaction for revert in case of error
+
     try {
-        /*        // format order data to create
-                       const formatOrder = {
-                           orderAmount: order.orderAmount,
-                           orderTotal: order.orderTotal,
-                           userId: order.userId
-                       }
+        // format order data to create
+        const formatOrder = {
+            orderAmount: order.orderAmount,
+            orderTotal: order.orderTotal,
+            userId: order.userId
+        }
 
-                       const newOrder = await Order.create(formatOrder); // create new order
-                       const getNewOrder = await Order.findByPk(newOrder.id) // get new order
-                       // format orderDetails data
-                       const formatOrderDetail = order.items.map(item => {
-                           return {
-                               orderId: getNewOrder.id,
-                               productsNameId: item.id,
-                               detailAmount: item.quantity,
-                               detailTotal: item.unit_price,
-                           }
-                       })
-                       await OrderDetail.bulkCreate(formatOrderDetail); // create orderDetails for new order
+        const newOrder = await Order.create(formatOrder, {transaction}); // create new order
+        const getNewOrder = await Order.findByPk(newOrder.id) // get new order
+        // format orderDetails data
+        const formatOrderDetail = order.items.map(item => {
+            return {
+                orderId: getNewOrder.id,
+                productsNameId: item.id,
+                detailAmount: item.quantity,
+                detailTotal: item.unit_price,
+            }
+        })
+        await OrderDetail.bulkCreate(formatOrderDetail, {transaction}); // create orderDetails for new order
 
-                       let allProductsPromises = []
-                       for (let i = 0; i < order.items.length; i++) {
-                           allProductsPromises.push(ProductsName.findByPk(order.items[i].id)) // array of products promises
-                       }
-                       const allProducts = await Promise.all(allProductsPromises); // get all products
+        let allProductsPromises = []
+        for (let i = 0; i < order.items.length; i++) {
+            allProductsPromises.push(ProductsName.findByPk(order.items[i].id)) // array of products promises
+        }
+        const allProducts = await Promise.all(allProductsPromises); // get all products
 
-                       let stockPromises = []
-                       for (let i = 0; i < allProducts.length; i++) {
-                           stockPromises.push(
-                               allProducts[i].update({stock: allProducts[i].stock - order.items[i].quantity}) // array update stock of products
-                           )
-                       }
-                       await Promise.all(stockPromises); // update stocks*/
+        let stockPromises = []
+        for (let i = 0; i < allProducts.length; i++) {
+            stockPromises.push(
+                allProducts[i].update({stock: allProducts[i].stock - order.items[i].quantity}, {transaction}) // array update stock of products
+            )
+        }
+        await Promise.all(stockPromises); // update stocks
 
         const dataToSend = {
             items: order.items,
@@ -68,8 +70,11 @@ const createOrder = async (order) => {
             }
         }
 
-        return await mercadoPago.preferences.create(dataToSend); // send data to mercadoPago - id property is necessary in frontend
+        const preference = await mercadoPago.preferences.create(dataToSend); // send data to mercadoPago - id property is necessary in frontend
+        await transaction.commit() // commit transaction in case of success
+        return preference
     } catch (error) {
+        await transaction.rollback() // rollback transaction in case of error
         return {error: error.message};
     }
 }
