e15b43c76b0de64a7818dcdbe819217f619c1313
Oscarcito122 <autlock64@gmail.com>
2023-05-12 11:54:22 -0300

    carrito ubicado

--- front/src/components/ProductsDetail/ProductsDetail.jsx
+++ front/src/components/ProductsDetail/ProductsDetail.jsx
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { allProductsId, addSToShoppingCart } from "../../redux/actions";
 import styles from "./ProductsDetail.module.css";
-import ShoppingCart from "../shoppingCart/ShoppingCart";
+import ShoppingCartImage from '../image/shoppingcart.svg'
+import ModalShoppingCart from "../modalShoppingCart/ModalShoppingCart";
+import SearchBar from "../searchbar/Searchbar";
 
 const image = "";
 
@@ -28,9 +30,31 @@ const ProductsDetail = () => {
   useEffect(() => {
     dispatch(allProductsId(id));
   }, [dispatch, id]);
+
+  const [isOpen, setIsOpen] = useState(false)
+  const openModal = () =>{setIsOpen(true) ; document.body.style.overflow = 'hidden';}
+  const closeModal = () =>{setIsOpen(false); document.body.style.overflow = 'auto';}
+  const shoppingCart = useSelector((state) => state.shoppingCart);
+
+  function showShoppingCart (){
+    openModal()
+  }
+
+  useEffect(()=>{
+    document.body.style.overflow = 'auto'
+  },[])
   
   return (
     <div >
+       <div className="carrito" onClick={showShoppingCart}>
+        <img src={ShoppingCartImage} alt="shopping-cart" width='25px' height='25px' />
+        <div style={{borderRadius:'50%', height:'25px', width:'25px', backgroundColor:'purple', display:'inline-flex', 
+                      justifyContent:'center', alignItems:'center', top:'-40px', left:'-45px'}}>
+          <span  style={{color:'white'}} >{shoppingCart.length}</span>
+        </div>
+      </div>
+      {<ModalShoppingCart isOpen={isOpen} closeModal={closeModal} />}
+      <SearchBar />
        <p>
       <Link to="/home">
         <button className={styles.btn1}>Go Henry Market</button>
--- front/src/components/categoryFilter/CategoryFilter.jsx
+++ front/src/components/categoryFilter/CategoryFilter.jsx
@@ -4,6 +4,11 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./categoryFilter.module.css";
 import FilterByPrice from "../orderByProducts/OrderByProducts";
+import ShoppingCartImage from '../image/shoppingcart.svg'
+import ModalShoppingCart from "../modalShoppingCart/ModalShoppingCart";
+import { useEffect } from "react";
+import SearchBar from "../searchbar/Searchbar";
+
 
 const CategoryFilter = () => {
   const dispatch = useDispatch();
@@ -16,12 +21,36 @@ const CategoryFilter = () => {
     dispatch(getProductsByCategory(category));
   };
 
+  const [isOpen, setIsOpen] = useState(false)
+  const openModal = () =>{setIsOpen(true) ; document.body.style.overflow = 'hidden';}
+  const closeModal = () =>{setIsOpen(false); document.body.style.overflow = 'auto';}
+  const shoppingCart = useSelector((state) => state.shoppingCart);
+
+  function showShoppingCart (){
+    openModal()
+  }
+
+  useEffect(()=>{
+    document.body.style.overflow = 'auto'
+  },[])
   return (
     <div className={styles.product}>
       <div className={styles.searchresults}>
         <Link to="/home">
           <button className={styles.btn}>Go Henry Market</button>
         </Link>
+       <div className="carrito" onClick={showShoppingCart}>
+        <img src={ShoppingCartImage} alt="shopping-cart" width='25px' height='25px' />
+        <div style={{borderRadius:'50%', height:'25px', width:'25px', backgroundColor:'purple', display:'inline-flex', 
+                      justifyContent:'center', alignItems:'center', top:'-40px', left:'-45px'}}>
+          <span  style={{color:'white'}} >{shoppingCart.length}</span>
+        </div>
+      </div>
+      {<ModalShoppingCart isOpen={isOpen} closeModal={closeModal} />}
+      
+
+      <SearchBar />
+      <p></p>
         <div className={styles.filter}>
           <label htmlFor="category-filter"></label>
           <select
@@ -53,16 +82,16 @@ const CategoryFilter = () => {
                     src={category.image}
                     alt={category.name}
                   />
-                  <h2 className={styles.cardTitle} style={{ color: "black" }}>
+                  <h2 className={styles.name} style={{ color: "black" }}>
                     {category.name}
                   </h2>
-                  <p className={styles.cardInfo} style={{ color: "black" }}>
+                  <p className={styles.description} style={{ color: "black" }}>
                     {category.description}
                   </p>
-                  <p className={styles.cardInfo} style={{ color: "black" }}>
+                  <p className={styles.price} style={{ color: "black" }}>
                     Stock: {category.stock} units
                   </p>
-                  <p className={styles.cardInfo} style={{ color: "black" }}>
+                  <p className={styles.price} style={{ color: "black" }}>
                     Price: USD {category.price}
                   </p>
                 </div>
