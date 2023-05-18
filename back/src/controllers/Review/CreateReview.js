d07d75ac2826f49d2c7ed41924a87f6cb930d6cf
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-05-18 09:40:27 -0400

    fix email to create comments

--- back/src/controllers/Review/CreateReview.js
+++ back/src/controllers/Review/CreateReview.js
@@ -1,8 +1,9 @@
 const {ProductsName, Review} = require("../../db");
+const {sendMailing} = require("../../utils/sendMailing");
 
 const createReview = async (req, res) => {
     try {
-        const {title, body, rating, productId} = req.body;
+        const {title, body, rating, productId, email} = req.body;
         const product = await ProductsName.findByPk(productId);
 
         const review = await Review.create({
@@ -12,6 +13,9 @@ const createReview = async (req, res) => {
             products_name_id: product.id,
             deleted: false
         });
+
+        await sendMailing(email, "Review Creada", "<h1>Review Creada exitosamente gracias por su tiempo</h1>")
+
         return res.json({review});
     } catch (error) {
         return res.status(500).json({error: error.message})
